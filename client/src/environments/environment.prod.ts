3bdcadc14aea5ff5b59e50ee25b202497606de58
taaqib-masood <taaqib.masood@icloud.com>
2026-03-03 16:15:32 +0530

    Update API URL for production environment

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    production: true,
-    apiUrl: 'https://ltts-test-management-backend.onrender.com/api'
+  production: true,
+  apiUrl: 'https://ltts-test-management-backend.onrender.com/api/auth'
 };
