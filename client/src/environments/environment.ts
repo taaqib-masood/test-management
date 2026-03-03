8a0ca58b07fde3896e69df35a154bc51993f56f7
taaqib-masood <taaqib.masood@icloud.com>
2026-03-03 14:53:22 +0530

    Update API URL for test environment

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:5000/api'
+  production: false,
+  apiUrl: 'https://ltts-test-management-backend.onrender.com/api'
 };
